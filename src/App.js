29a45ed7cd0fce323428c1884f7a241f33c5072b
Saurabhvermaji <gaminggalaxy2022@gmail.com>
2025-08-28 12:59:31 +0530

    Fixed white page issue with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import AppRoutes from "./routes/routes";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter >
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter >
   );
 };
 
